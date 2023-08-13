126ac1ea61755c31c0df1198f2204d83011334bd
MohammadJavad14 <mrph14@gmail.com>
2023-08-13 17:07:38 +0330

    testDBRepo added

--- internal/repository/dbrepo/dbrepo.go
+++ internal/repository/dbrepo/dbrepo.go
@@ -13,11 +13,23 @@ type postgresDBRepo struct {
 	DB  *gorm.DB
 }
 
+type testDBRepo struct {
+	App *config.AppConfig
+	DB  *gorm.DB
+}
+
 // NewPostgresRepo creates the repository
 func NewPostgresRepo(Conn *gorm.DB, a *config.AppConfig) repository.DatabaseRepo {
 	app = a
 	return &postgresDBRepo{
 		App: a,
 		DB:  Conn,
+	}
+}
+
+func NewTestRepo(Conn *gorm.DB, a *config.AppConfig) repository.DatabaseRepo {
+	app = a
+	return &testDBRepo{
+		App: a,
 	}
 }
